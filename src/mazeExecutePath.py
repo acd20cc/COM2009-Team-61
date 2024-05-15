14928195a8c8243f4718536daba4382efc8c69c6
Seunghyun Im <sim6@sheffield.ac.uk>
2024-05-15 23:17:36 +0100

    twenty seconds map saver

--- src/mazeExecutePath.py
+++ src/mazeExecutePath.py
@@ -2,7 +2,11 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+from math import degrees
 import numpy as np
+
 
 class MazeSolver:
 
@@ -12,7 +16,8 @@ class MazeSolver:
         self.rate = rospy.Rate(10)
 
         self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.laser_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+        self.laser_sub = rospy.Subscriber('/scan', LaserScan, self.laser_callback)
+        self.odometry_sub = rospy.Subscriber('/odom', Odometry, self.odom_cb)
 
         # maximise the default velocity
         self.max_velocity = 0.26
@@ -33,8 +38,8 @@ class MazeSolver:
         # default for stopping the code
         self.ctrl_c = False
 
-        # Set to store visited locations
-        self.visited_locations = set()  
+        # store completed locations in to set
+        self.visited = set()  
 
         rospy.on_shutdown(self.shutdown_hook)
 
@@ -43,7 +48,7 @@ class MazeSolver:
         self.cmd_vel_pub.publish(Twist())
         self.ctrl_c = True
 
-    def scan_callback(self, scan_data):
+    def laser_callback(self, scan_data):
         # Extract the the last 90 measurements demonstrate right side arc
         right_arc = scan_data.ranges[-90:]
         # Extract the first 91 measurements demonstrate the left side arc.
@@ -58,14 +63,34 @@ class MazeSolver:
         #Set array left side between 30 to 60
         self.left_dist = np.array(front_arc[30:60]).min()
 
+    def odom_cb(self, odom_data):
+        orientation = odom_data.pose.pose.orientation
+        position = odom_data.pose.pose.position
+        (_, _, yaw) = euler_from_quaternion([orientation.x,
+            orientation.y, orientation.z, orientation.w],'sxyz')
+        
+        self.yaw = self.round(degrees(yaw), 4)
+        self.posx = self.round(position.x, 4)
+        self.posy = self.round(position.y, 4)
+
+    def round(self, value, precision):
+        value = int(value * (10**precision))
+        return float(value) / (10**precision)
+
+
     def move(self):
+        """
+        Default Maze-solve algorithm
+        """
         while not self.ctrl_c:
-            min_dist = self.front_dist > 0.39
+            min_dist = self.front_dist > 0.41
 
             # If the distance of right side is less than 0.3 and distance front side is bigger than 0.39 turn right
             # right_distance is used to prioritise the right side from two ways.
             if self.right_dist < 0.3 and min_dist:
                 print("Chose to turn Left")
+                print(self.posx)
+                print(self.posy)
                 self.vel.linear.x = self.max_velocity 
                 self.vel.angular.z = 1
                 self.decision = "left"
@@ -73,6 +98,8 @@ class MazeSolver:
             # If the distance on the right is between 0.3 and 0.4 units and min_dist is true, proceed forward.
             elif 0.3 < self.right_dist < 0.4 and min_dist:
                 print("Chose to go front")
+                print(self.posx)
+                print(self.posy)
                 self.vel.linear.x = self.max_velocity
                 self.vel.angular.z = 0
                 self.decision = "forward"
@@ -80,6 +107,8 @@ class MazeSolver:
             # If right side is open, turn to the right side
             elif self.right_dist > 0.3 and min_dist:
                 print("Chose to turn right")
+                print(self.posx)
+                print(self.posy)
                 self.vel.linear.x = self.max_velocity
                 self.vel.angular.z = -1
                 self.decision = "right"
@@ -102,17 +131,19 @@ class MazeSolver:
         """
         # Mark forward as visited
         if self.decision == "forward":
-            self.visited_locations.add("forward") 
+            self.visited.add("forward") 
         # Mark left as visited
         elif self.decision == "left":
-            self.visited_locations.add("left") 
+            self.visited.add("left") 
         # Mark right as visited
         elif self.decision == "right":
-            self.visited_locations.add("right")  
+            self.visited.add("right")  
         # Mark backward as visited
         elif self.decision == "backward":
-            self.visited_locations.add("backward")
+            self.visited.add("backward")
         self.cmd_vel_pub.publish(self.vel)
+        
+
 
 
     def main(self):
